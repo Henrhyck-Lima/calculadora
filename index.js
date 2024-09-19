bb99676af6a80c7a8989ce6039d88792d4789d32
Henrhyck Lima <115596103+Henrhyck-Lima@users.noreply.github.com>
2024-09-19 20:49:20 -0300

    Indentação

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 
-        function somar() {
-            let v1 = document.querySelector('input#valor1');
-            let v2 = document.querySelector('input#valor2');
-            let resultado = document.querySelector('div#resultado');
-            let input1 = Number(v1.value);
-            let input2 = Number(v2.value);
-            let so = input1 + input2;
+function somar() {
+    let v1 = document.querySelector('input#valor1');
+    let v2 = document.querySelector('input#valor2');
+    let resultado = document.querySelect('div#resultado');
+    let input1 = Number(v1.value);
+    let input2 = Number(v2.value);
+    let so = input1 + input2;
 
-            resultado.innerHTML = `${so}`
-        }
-        function subtrair(){
-            let v1 = document.querySelector('input#valor1');
-            let v2 = document.querySelector('input#valor2');
-            let resultado = document.querySelector('div#resultado');
-            let input1 = Number(v1.value);
-            let input2 = Number(v2.value);
-            let su = input1 - input2;
+    resultado.innerHTML = `${so}`
+}
 
-            resultado.innerHTML = `${su}`
-        }
-        function dividir(){
-            let v1 = document.querySelector('input#valor1');
-            let v2 = document.querySelector('input#valor2');
-            let resultado = document.querySelector('div#resultado');
-            let input1 = Number(v1.value);
-            let input2 = Number(v2.value);
-            let di = input1 / input2;
+function subtrair() {
+    let v1 = document.querySelector('input#valor1');
+    let v2 = document.querySelector('input#valor2');
+    let resultado = document.querySelect('div#resultado');
+    let input1 = Number(v1.value);
+    let input2 = Number(v2.value);
+    let su = input1 - input2;
 
-            resultado.innerHTML = `${di}`
-        }
-        function multiplicar(){
-            let v1 = document.querySelector('input#valor1');
-            let v2 = document.querySelector('input#valor2');
-            let resultado = document.querySelector('div#resultado');
-            let input1 = Number(v1.value);
-            let input2 = Number(v2.value);
-            let mu = input1 * input2;
+    resultado.innerHTML = `${su}`
+}
 
-            resultado.innerHTML = `${mu}`
-        }
+function dividir() {
+    let v1 = document.querySelector('input#valor1');
+    let v2 = document.querySelector('input#valor2');
+    let resultado = document.querySelect('div#resultado');
+    let input1 = Number(v1.value);
+    let input2 = Number(v2.value);
+    let di = input1 / input2;
+
+    resultado.innerHTML = `${di}`
+}
+
+function multiplicar() {
+    let v1 = document.querySelector('input#valor1');
+    let v2 = document.querySelector('input#valor2');
+    let resultado = document.querySelect('div#resultado');
+    let input1 = Number(v1.value);
+    let input2 = Number(v2.value);
+    let mu = input1 * input2;
+
+    resultado.innerHTML = `${mu}`
+}
